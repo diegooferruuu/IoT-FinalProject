40373ed5de1a651bc87f59e71bcc31d0488daeff
diegooferruuu <diego.ferrufino@ucb.edu.bo>
2024-12-16 11:42:05 -0400

    Arreglo

--- rules/tempState_db_rule.sql
+++ rules/tempState_db_rule.sql
@@ -2,4 +2,4 @@ SELECT state.reported.temperatureState AS temperatureState, state.reported.incub
 topic(3) AS thing_name, substring(topic(3), 10) AS version 
 FROM '$aws/things/+/shadow/update/accepted' 
 WHERE substring(topic(3), 0, 9) = 'incubator' AND state.reported.temperatureState >= 0 AND state.reported.temperatureState <= 2 
-`AND state.reported.incubatorMode >= 0 AND state.reported.incubatorMode <= 1
+AND state.reported.incubatorMode >= 0 AND state.reported.incubatorMode <= 1
